63ca84117709b484930396c5a68d654e8ba4c0f0
Ginsm <mattg.0609@gmail.com>
2024-03-25 06:36:40 -0400

    Bind autosuggest-accept to ctrl + space
    autosuggest-accept accepts the suggestion from zsh-autosuggestion.

--- zsh/modules/keybinds.zsh
+++ zsh/modules/keybinds.zsh
@@ -5,3 +5,6 @@ bindkey "^[[1;5D" backward-word
 # Similar to above, but with alt.
 bindkey "^[[1;3C" forward-word
 bindkey "^[[1;3D" backward-word
+
+# Bind space to accept zsh-autocompletion suggestion
+bindkey '^@' autosuggest-accept
