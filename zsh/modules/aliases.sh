8f0d3d55019f78383b9aa32c98b9e00e6f2bf114
Ginsm <mattg.0609@gmail.com>
2025-01-22 05:50:03 -0500

    Remove function from aliases.sh

--- zsh/modules/aliases.sh
+++ zsh/modules/aliases.sh
@@ -42,19 +42,6 @@ alias pip='pip3.12'
 # Open location in explorer
 alias open='explorer.exe'
 
-# Change wallpaper in WSL using fzf
-function chwall() {
-  local image_path="${1:-$WSL_BACKGROUNDS_PATH}"
-  # Use null character as delimiter for fzf output
-  selected_file=$(ls -1 "$image_path" | fzf --query=".gif | .jpg | .jpeg | .png | .webp " --reverse --border=sharp --margin=1% --padding=1% --header="Choose a wallpaper" --header-first --no-info --print0)
-
-  # Check if a file was selected
-  if [ -n "$selected_file" ]; then
-    # Use null character as delimiter for xargs input
-    echo "$selected_file" | xargs -0 change-background
-  fi
-}
-
 # Source any local aliases.
 if [ -f "$ALIASES.local" ]; then
   . "$ALIASES.local"
